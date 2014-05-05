8390dbeaaf08fe025d2ce8e63ede40881f62193e
sgf <sgf.dma@gmail.com>
2014-05-05 11:45:39 +0400

    xmonad. No borders in Full layout.

--- src/xmonad.hs
+++ src/xmonad.hs
@@ -23,7 +23,7 @@ main                = do
       . alterKeys myKeys
       $ defaultConfig
           { manageHook = manageDocks <+> manageHook defaultConfig
-          , layoutHook = smartBorders . avoidStruts  $  layoutHook defaultConfig
+          , layoutHook = avoidStruts $ layout
           , logHook    = dynamicLogWithPP xmobarPP
                            { ppOutput = hPutStrLn xmPipe
                            , ppTitle  = xmobarColor "green" ""
@@ -34,6 +34,19 @@ main                = do
           , terminal = "xterm -fg black -bg white"
           --, layoutHook = smartBorders $ layoutHook xfceConfig
           }
+
+-- Layouts definition from defaultConfig with Full layout without borders.
+layout = tiled ||| Mirror tiled ||| noBorders Full
+  where	
+    -- default tiling algorithm partitions the screen into two panes
+    tiled   = Tall nmaster delta ratio
+    -- The default number of windows in the master pane
+    nmaster = 1
+    -- Default proportion of screen occupied by master pane
+    ratio   = 1/2
+    -- Percent of screen to increment by when resizing panes
+    delta   = 3/100
+
 
 -- Run trayer.
 runTrayer :: IO ProcessHandle
